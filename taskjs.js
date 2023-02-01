53676b1ea492f3116b1faf1d379494f62411a0c4
Kayathri2218 <kaya3kayu2000@gmail.com>
2023-02-01 13:09:19 -0500

    adding 1st javascript experssion

---
+++ taskjs.js
@@ -0,0 +1,156 @@
+
+//Find Addition . 1
+
+document.write('<br>');
+document.write("Addition : ");
+var F = 3;
+var E = 47;
+var D = F+E;
+console.log(D);
+document.write(D);
+document.write('<br>');
+document.write('<br>');
+
+//Calculation Subraction . 2
+
+document.write("Subraation : ");
+var A = 2;
+var B = 5;
+var C = A-B;
+console.log(C);
+document.write(C);
+document.write('<br>');
+document.write('<br>');
+
+//calculation multiplication . 3
+
+document.write("Multiplication : ");
+var a = 5;
+var b = 5;
+var c = a*B;
+console.log(c);
+document.write(c);
+document.write('<br>');
+document.write('<br>');
+
+//caculation division . 4
+
+document.write("Divition : ")
+var e = 10;
+let f = 5;
+let d = e/F;
+console.log(d);
+document.write(d);
+document.write('<br>');
+document.write('<br>');
+
+//Area of circle . 5
+
+document.write("Area of circle : ");
+var R = 3;
+console.log(Math.PI*R**2);
+document.write(Math.PI*R**2);
+document.write('<br>');
+document.write('<br>');
+
+//Area of Triangle . 6
+
+document.write("Area of Triangle : ");
+const Bi = 5;
+const Hi = 4;
+console.log((Bi/Hi)/2);
+document.write((Bi/Hi)/2);
+document.write('<br>');
+document.write('<br>');
+
+//simple interest . 7
+
+document.write("simple interest : ");
+var p = 500;
+var t = 5;
+var r = 6;
+console.log((p*t*r)/100);
+document.write((p*t*r)/100);
+document.write('<br>');
+document.write('<br>');
+
+//calculation square . 8
+
+document.write("Calculation square : ");
+console.log(5**2);
+document.write(5**2);
+document.write('<br>');
+document.write('<br>');
+
+//calculation cube . 9
+
+document.write("Calculation Cube : ");
+const Q = 4;
+console.log(Q**3);
+document.write(Q**3);
+document.write('<br>');
+document.write('<br>');
+
+//total and average . 10
+
+document.write("Total and Average : ");
+var sum1 = 30;
+var sum2 = 30;
+var sum3 = 40;
+var I = sum1+sum2+sum3;
+console.log(I);
+document.write(I);
+console.log(I/3*100);
+document.write(I/3*100);
+document.write('<br>');
+document.write('<br>');
+
+//prime or not . 11
+
+
+//large number . 12
+
+document.write("Large Number : ");
+var N = 10;
+var K =20;
+console.log(N<K);
+document.write(N<K);
+document.write('<br>');
+document.write('<br>');
+
+//positive or negative . 13
+
+document.write("Positive or Negative : ");
+var n = 10;
+console.log(n<0);
+document.write(n<0);
+document.write('<br>');
+document.write('<br>');
+
+//greater than 10 . 14
+
+document.write("Greater then 10 : ");
+var num = 20
+console.log(num>10);
+document.write(num>10);
+document.write('<br>');
+document.write('<br>');
+
+//even or odd . 15
+
+document.write("Even or odd : ")
+var even = 10;
+var odd = 7;
+console.log(even/2);
+document.write(even/2);
+console.log(odd/2);
+document.write('<br>');
+document.write('<br>');
+
+//divisible by 5 . 16
+
+document.write("Divisible by 5 : ")
+var Num = 15;
+console.log(Num/5);
+document.write(Num/5);
+document.write('<br>');
